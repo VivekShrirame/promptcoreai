2cb8fb6f0461fa194da8bf6cafb834b61e6e8caf
Viveks610 <vivek.shrirame123@gmail.com>
2023-05-11 18:20:59 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
-      <p className="text-center">
+      <p className="desc text-center">
         PromptCoreAI is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
